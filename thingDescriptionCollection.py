a7fa9fe75248ff9e2375abbe5e8dc662aacf0b04
warburec <77669019+warburec@users.noreply.github.com>
2025-08-23 20:20:01 +0100

    cleanup for thingDescriptionCollection

--- thingDescriptionCollection.py
+++ thingDescriptionCollection.py
@@ -1,60 +1,86 @@
 class ThingDescriptionCollection:
     tds = None
     
-    def __init__(self,tds):
+    def __init__(self, tds):
         self.tds = tds
 
     def getActionInput(self, actionName):
-        for i in self.tds:
-            if "actions" in i:
-                for k,v in i["actions"].items():
-                    if k == actionName:
-                        print("k: ", k)
-                        return v["input"]
+        for td in self.tds:
+            if "actions" not in td:
+                continue
+
+            for k, v in td["actions"].items():
+                if k == actionName:
+                    return v["input"]
                 
     def getEventData(self, eventName):
         for i in self.tds:
-            if "events" in i:
-                for k,v in i["events"].items():
-                    if k == eventName:
-                        if "description" in v["data"]:
-                            del v["data"]["description"]
-                        if "unit" in v["data"]:
-                            del v["data"]["unit"]
-                        if "enum" in v["data"]:
-                            del v["data"]["enum"]
-                        if "properties" in v["data"]:
-                            for j in v["data"]["properties"].values():
-                                if "description" in j:
-                                    del j["description"]
-                                if "unit" in j:
-                                    del j["unit"]
-                                if "enum" in j:
-                                    del j["enum"]
-                        return v["data"]
+            if "events" not in i:
+                continue
+
+            for k, v in i["events"].items():
+                if k != eventName:
+                    continue
+
+                if "description" in v["data"]:
+                    del v["data"]["description"]
+
+                if "unit" in v["data"]:
+                    del v["data"]["unit"]
+
+                if "enum" in v["data"]:
+                    del v["data"]["enum"]
+
+                if "properties" not in v["data"]:
+                    return v["data"]
+                
+                for j in v["data"]["properties"].values():
+                    if "description" in j:
+                        del j["description"]
+
+                    if "unit" in j:
+                        del j["unit"]
+
+                    if "enum" in j:
+                        del j["enum"]
+                
+                return v["data"]
                 
     def getActionOutput(self, actionName):
-        for i in self.tds:
-            if "actions" in i:
-                for k,v in i["actions"].items():
-                    if k == actionName:
-                        print("k: ", k)
-                        if "output" in v:
-                            if "description" in v["output"]:
-                                del v["output"]["description"]
-                            if "enum" in v["output"]:
-                                del v["output"]["enum"]
-                            return v["output"]
+        for td in self.tds:
+            if "actions" not in td:
+                continue
+
+            for k, v in td["actions"].items():
+                if k != actionName:
+                    continue
+                
+                if "output" not in v:
+                    continue
+
+                if "description" in v["output"]:
+                    del v["output"]["description"]
+
+                if "enum" in v["output"]:
+                    del v["output"]["enum"]
+                    
+                return v["output"]
         return None
     
-    def getPropertyValue(self,propertyName):
-        for i in self.tds:
-            if "properties" in i:
-                for k,v in i["properties"].items():
-                    if k == propertyName:
-                        a = {}
-                        a["type"] = v["type"]
-                        if "properties" in v:
-                            a["properties"] = v["properties"]
-                        return a
+    def getPropertyValue(self, propertyName):
+        for td in self.tds:
+            if "properties" not in td:
+                continue
+
+            for k, v in td["properties"].items():
+                if k != propertyName:
+                    continue
+
+                a = {}
+                a["type"] = v["type"]
+
+                if "properties" in v:
+                    a["properties"] = v["properties"]
+                
+                return a
         return None
